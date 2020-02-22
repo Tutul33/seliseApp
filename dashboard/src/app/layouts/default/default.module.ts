fa31ac6c23d15f00d00578e0682e024b4f513674
Tutul33 <tutulcou@gmail.com>
2020-02-22 15:55:59 +0600

    test

--- dashboard/src/app/layouts/default/default.module.ts
+++ dashboard/src/app/layouts/default/default.module.ts
@@ -4,16 +4,34 @@ import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { DashboardService } from 'src/app/modules/dashboard.service';
 
 @NgModule({
   declarations: [
     DefaultComponent,
     DashboardComponent,
-    PostsComponent
+    PostsComponent 
   ],
   imports: [
     CommonModule,
     RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule
+  ],
+  providers:[
+    DashboardService
   ]
 })
 export class DefaultModule { }
